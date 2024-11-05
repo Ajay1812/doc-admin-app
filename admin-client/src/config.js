ddce03b9dbd316a17687ad81d188145f0ea0cd30
Ajay1812 <a.kumar01c@gmail.com>
2024-11-05 10:01:00 +0530

    .

--- admin-client/src/config.js
+++ admin-client/src/config.js
@@ -1,3 +1,2 @@
-export const BASE_URL = 'http://localhost:3000'
-// export const BASE_URL = 'https://backend-doc-admin.codebomb.co.in:3000'
-// export const BASE_URL = 'https://dantveda-admin.netlify.app:3000'
+// export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://doc-admin.codebomb.co.in:3000'
